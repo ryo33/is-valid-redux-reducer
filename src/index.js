fffe1835f6c3006cc49dd0a7acb881ff8e5dc81a
Ryo Hashiguchi <ryo12redstone@gmail.com>
2016-05-26 22:07:58 +0900

    Add files

---
+++ src/index.js
@@ -0,0 +1,34 @@
+function randomType() {
+  return '@@is-valid-redux-reducer/' + Math.random().toString(36).substring(7).split('').join('\\')
+}
+
+export default function isValidReduxReducer(reducer, throwError = false) {
+  if (typeof reducer !== 'function') {
+    if (throwError) {
+      throw new Error('Reducer must be a function.')
+    } else {
+      return false
+    }
+  }
+
+  const initialState = reducer(undefined, {type: randomType()})
+  if (typeof initialState === 'undefined') {
+    if (throwError) {
+      throw new Error('Reducer must return the initial state if the state is undefined.')
+    } else {
+      return false
+    }
+  }
+
+  const state = []
+  const newState = reducer(state, {type: randomType()})
+  if (state !== newState) {
+    if (throwError) {
+      throw new Error('Reducer must return the current state for any unknown actions.')
+    } else {
+      return false
+    }
+  }
+
+  return true
+}
---
+++ test/index.spec.js
@@ -0,0 +1,36 @@
+import { expect } from 'chai'
+import isValidReduxReducer from '../src/index.js'
+
+describe('isValidReduxReducer', () => {
+  const reducerA = {}
+  it('should return false when the reducer is not a function', () => {
+    expect(isValidReduxReducer(reducerA)).to.be.false
+  })
+  it('should throw a error when the reducer is not a function', () => {
+    expect(() => isValidReduxReducer(reducerA, true)).to.throw(Error, 'Reducer must be a function.');
+  })
+
+  const reducerB = (state, action) => state
+  it('should return false when the reducer does not return the initial state', () => {
+    expect(isValidReduxReducer(reducerB)).to.be.false
+  })
+  it('should throw a error when the reducer does not return the initial state', () => {
+    expect(() => isValidReduxReducer(reducerB, true)).to.throw(Error, 'Reducer must return the initial state if the state is undefined.');
+  })
+
+  const reducerC = (state, action) => ({})
+  it('should return false when the reducer does not return the current state', () => {
+    expect(isValidReduxReducer(reducerC)).to.be.false
+  })
+  it('should throw a error when the reducer does not return the current state', () => {
+    expect(() => isValidReduxReducer(reducerC, true)).to.throw(Error, 'Reducer must return the current state for any unknown actions.');
+  })
+
+  const reducerD = (state = {}, action) => state
+  it('should return true when the reducer is valid', () => {
+    expect(isValidReduxReducer(reducerD)).to.be.true
+  })
+  it('should not throw a error when the reducer is valid', () => {
+    expect(() => isValidReduxReducer(reducerD, true)).not.to.throw(Error);
+  })
+})
